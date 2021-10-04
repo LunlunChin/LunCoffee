70dba1fd262ac2da6d029b211c66435abc1fab2a
Lun <zhielun999@gmail.com>
2021-10-04 14:23:29 +0800

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'luncoffee.ionic',
+  appName: 'lun_coffee',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
